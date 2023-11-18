ac4d34183ebc1d3866a842f4a4ed2808795f4edb
Денисов Кирилл Юрьевич <kirill@ex.ru>
2023-11-18 23:59:08 +0300

    Add maybe.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/denilai/maybe
+
+go 1.21.1
---
+++ maybe.go
@@ -0,0 +1,32 @@
+package maybe
+
+import "fmt"
+
+type Maybe[T any] struct {
+	value    T
+	hasValue bool
+}
+
+func (m Maybe[T]) String() string {
+	if m.HasValue() {
+		return fmt.Sprintf("Just %v", m.Value())
+	} else {
+		return "Nothing"
+	}
+}
+
+func (m Maybe[T]) HasValue() bool {
+	return m.hasValue
+}
+
+func (m Maybe[T]) Value() T {
+	if m.hasValue {
+		return m.value
+	} else {
+		panic("Nothing")
+	}
+}
+
+func Just[T any](val T) Maybe[T] { return Maybe[T]{value: val, hasValue: true} }
+
+func Nothing[T any]() Maybe[T] { return Maybe[T]{hasValue: false} }
